8c4a61659cb2739458e4250f27de0b3881001223
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-01-05 20:53:52 +0200

    Add two squares chacing each other

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function moveSquare(i = 0) {
-  const yPosition = Math.sin(i) * 100;
-  document.getElementById('square').style.transform = `translateY(${yPosition}px)`;
+  document.getElementById('square1').style.transform = `translateY(${Math.sin(i) * 100}px)`;
+  document.getElementById('square2').style.transform = `translateY(${Math.cos(i) * 100}px)`;
   setTimeout(() => moveSquare(i + 0.1), 50);
 }
 
