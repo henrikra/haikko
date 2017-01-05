5d0ad218c1c2d2db3e3f7e821a3b42119e625708
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-01-05 20:39:06 +0200

    Refactor to be more clear

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function f(i) {
+function moveSquare(i = 0) {
   const yPosition = Math.sin(i) * 100;
   document.getElementById('square').style.transform = `translateY(${yPosition}px)`;
-  setTimeout(() => f(i + 0.1), 50);
+  setTimeout(() => moveSquare(i + 0.1), 50);
 }
-f(0);
+
+function startFloating() {
+  moveSquare();
+}
+
+startFloating();
