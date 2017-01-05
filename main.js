f386a2f30c03fd41050d67ef5f8765d3c6c8b52c
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-01-05 22:11:38 +0200

    Add stuff for jekku

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+function _(selector) {
+  return document.getElementById(selector);
+}
+
 let i = 0;
 
 function moveSquare() {
   const offset = 3;
-  document.getElementById('square1').style.transform = `translate(${Math.sin(i) * 100}px, ${Math.sin(i) * 100 * offset}px)`;
-  document.getElementById('square2').style.transform = `translate(${Math.cos(i) * 100 * offset}px, ${Math.cos(i) * 100}px)`;
+  _('emoji1').style.transform = `translate(${Math.sin(i) * 100}px, ${Math.sin(i) * 100 * offset}px)`;
+  _('emoji2').style.transform = `translate(${Math.cos(i) * 100 * offset}px, ${Math.cos(i) * 100}px)`;
   i += 0.05;
 
   requestAnimationFrame(moveSquare);
@@ -13,4 +17,12 @@ function startFloating() {
   moveSquare();
 }
 
-startFloating();
+
+_('start-btn').addEventListener('click', () => {
+  startFloating();
+  _('main-image').src = 'http://travelwithtom.com/wp-content/uploads/2014/01/kracow-poland-slider1.jpg';
+  _('main-text').style.display = 'none';
+  _('reveal').style.display = 'block';
+  _('emojis').style.display = 'block';
+  _('start-btn').style.display = 'none';
+});
