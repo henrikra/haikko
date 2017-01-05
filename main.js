a735ca69da1e24d901074c94bdca55b460450c79
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-01-05 21:30:41 +0200

    Add offset

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let i = 0;
 
 function moveSquare() {
-  document.getElementById('square1').style.transform = `translate(${Math.sin(i) * 100}px, ${Math.sin(i) * 100}px)`;
-  document.getElementById('square2').style.transform = `translate(${Math.cos(i) * 100}px, ${Math.cos(i) * 100}px)`;
+  const offset = 3;
+  document.getElementById('square1').style.transform = `translate(${Math.sin(i) * 100}px, ${Math.sin(i) * 100 * offset}px)`;
+  document.getElementById('square2').style.transform = `translate(${Math.cos(i) * 100 * offset}px, ${Math.cos(i) * 100}px)`;
   i += 0.05;
 
   requestAnimationFrame(moveSquare);
