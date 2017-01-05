1e8dea2048d4aa57a44f6c180ba77058e96dfc0f
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-01-05 21:26:49 +0200

    Use requestAnimationFrame to get animation to 2017

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-function moveSquare(i = 0) {
+let i = 0;
+
+function moveSquare() {
   document.getElementById('square1').style.transform = `translate(${Math.sin(i) * 100}px, ${Math.sin(i) * 100}px)`;
   document.getElementById('square2').style.transform = `translate(${Math.cos(i) * 100}px, ${Math.cos(i) * 100}px)`;
-  setTimeout(() => moveSquare(i + 0.1), 50);
+  i += 0.05;
+
+  requestAnimationFrame(moveSquare);
 }
 
 function startFloating() {
